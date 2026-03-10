ec5e06bffa24e4bce927d07c5ad84c368dbb80cf
Trang <nguyenminhtrang9213@gmail.com>
2026-03-10 19:55:30 +0100

    add component and setup layout

---
+++ components/EmailInput.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function EmailInput() {
+  return (
+    <div>
+      <input type="text" placeholder="Email address" />
+      <button>Sign Up</button>
+    </div>
+  );
+}
